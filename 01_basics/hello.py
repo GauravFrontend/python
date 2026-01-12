957be4662f04345316641983d9ffa3fdeff51efc
GauravFrontend <gaurav9911093928@gmail.com>
2026-01-12 06:52:36 +0530

    dataTypes

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -1,2 +1,15 @@
+import math
+import random
 print("Hello World")
 print("hellow world")
+x = 10
+y = x
+print(y)
+x = 15
+y = x
+print(y,x)
+mylist = [1,2,3,["a","b"]]
+print(mylist)
+
+print(math.pi, "value of pi")
+print(random.random(), "a random number")
